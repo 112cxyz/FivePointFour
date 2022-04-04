c4e8e8558e4bc5af3ce98c7ca8dfb8a1be743dd4
112c <tobyfox2020@gmail.com>
2022-04-04 14:56:06 +0000

    Created Everything

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # FivePointFour
 5.4 Review
+
+The default user is username and password is passwd
+
+stdiomask is needed use pip install stdiomask or pip3 install stdiomask
---
+++ main.py
@@ -0,0 +1,44 @@
+#5.4 Review. Authentication Review
+#Create a Password Authenticator that gives you three tries with an user account
+import os
+import stdiomask
+#stdiomask is a input hider so text isnt shown.
+#if not installed use pip install stdiomask or pip3 install stdiomask
+cor=0
+username = input("Welcome Please Put Your UserName Here\n>>> ")
+
+if os.path.exists(username):
+    for i in range(0,3):
+        f = open(username, "r")
+        print("Enter Password")
+        if stdiomask.getpass(">>> ") == f.read():
+            cor=1
+            break
+        else:
+            print("incorect")
+    if cor == 1:
+        print("Welcome")
+    elif cor == 0:
+        print("Too many incorect tries")
+else:
+    print("User Does Not Exist")
+    print("Would you like to create "+username+"? [y/n] ")
+    passing = input(">>> ")
+    if passing == "y":
+        print("Enter Password")
+        password = stdiomask.getpass(">>> ")
+        b = open(username, "w")
+        b.write(password)
+        b.close()
+        os.system("clear")
+        import main
+    elif passing == "Y":
+        print("Enter Password")
+        password = stdiomask.getpass(">>> ")
+        b = open(username, "w")
+        b.write(password)
+        b.close()
+        os.system("clear")
+        import main
+    else:
+        print("Bye!")
